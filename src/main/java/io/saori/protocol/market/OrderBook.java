0bccfb9dc767713b9bfac3356b02310150e87cbd
Saori Isabel Tovar Niño <saorisabeltovar@gmail.com>
2026-03-31 19:09:11 -0500

    Professionally modularing my Order Book and Market Prices File System and Packages done

--- src/main/java/io/saori/protocol/market/OrderBook.java
+++ src/main/java/io/saori/protocol/market/OrderBook.java
@@ -1,5 +1,12 @@
 package io.saori.protocol.market;
 
+import io.saori.protocol.core.Product;
+
 public class OrderBook {
-    
+    public void processOrder(Order order, Product product) {
+        // This is where the 10 -> 6 logic will live!
+        if (order.getType() == Order.Type.SELL) {
+            // Logic to add to the book
+        }
+    }
 }
